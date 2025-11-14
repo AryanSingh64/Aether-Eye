a540ca24964bd5877b5cc3320b5908c851bdf452
Aryan Singh <93329923+Warlord64@users.noreply.github.com>
2025-11-14 14:24:42 +0530

    Aether Eye Version 01

---
+++ Arduino/ESP-32 Cam/board_config.h
@@ -0,0 +1,13 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+// ===================
+// Select camera model
+// ===================
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+// #define CAMERA_MODEL_ESP32S3_EYE
+
+
+#include "camera_pins.h"
+
+#endif  // BOARD_CONFIG_H
